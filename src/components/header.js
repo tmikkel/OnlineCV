1dfbeafc73fb37783910e53e23d92885f7b3295d
Tobias_Schleiss <tsandreassen@outlook.dk>
2025-09-21 00:56:26 +0200

    updated to next link

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,6 @@
 "use client";
+import Link from "next/link";
+
 function scrollToElement(id) {
   const element = document.getElementById(id);
   if (element) {
@@ -11,7 +13,7 @@ export const Header = () => {
     <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-[48%] lg:flex-col lg:justify-between lg:py-24">
       <div>
         <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
-          <a href="/">Tobias Schleiss-Andreassen</a>
+          <Link href="/">Tobias Schleiss-Andreassen</Link>
         </h1>
         <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
           MSc Student in Computer Science & Engineering
